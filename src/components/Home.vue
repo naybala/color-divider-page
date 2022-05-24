91fb3544555f46874ed0bd8c7233f56fb94847ca
naybala <naybalaismylife.nbl@gmail.com>
2022-05-24 18:42:35 +0630

    some testing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,50 @@
 <template>
-  <div>
-    <section id="home">
-      <div>
-        <br /><br /><br />
-        <h1 class="text-center">Home Section</h1>
-        <br />
-        <br /><br /><br /><br />
-        <br />
-
-        <br /><br /><br /><br />
-        <br />
-        <br /><br /><br /><br />
-        <br />
-        <br /><br /><br /><br />
-        <br />
-        <br /><br /><br /><br />
-        <br />
-
-        <br /><br /><br /><br />
-        <br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br />
-        <br />
+  <div class="home">
+    <div class="home1">
+      <div class="home2">
+        <section id="home">
+          <div class="bgimages">
+            <div class="heroText">
+              <h1 class="text-white mb-lg-5">Home Section</h1>
+            </div>
+          </div>
+          <br /><br /><br /><br />
+        </section>
       </div>
-    </section>
+      <br /><br />
+    </div>
+    <br /><br />
   </div>
 </template>
+
+<script></script>
+<style scoped>
+.home {
+  background-color: aqua;
+}
+.home1 {
+  background-color: blueviolet;
+}
+.home2 {
+  background-color: brown;
+}
+/* bg with image  */
+.bgimages {
+  width: 100%;
+  height: 420px;
+  background: url("../../src/assets/images/artwork.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+}
+
+.heroText {
+  position: absolute;
+  z-index: 9;
+  top: 120%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: auto;
+}
+</style>
